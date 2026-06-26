ab9d2184ab058c42ad187a98e41fbdd0403be040
aaqib <aaqibuddinhashmi@gmail.com>
2026-06-26 12:30:52 +0530

    Restore construction presentation and embed live dashboard in demo section

--- src/components/ConstructionModal.jsx
+++ src/components/ConstructionModal.jsx
@@ -15,8 +15,8 @@ const ConstructionModal = ({ isOpen, onClose }) => {
         </button>
         <div className="construction-modal-content">
           <iframe 
-            src="/construction-live/index.html" 
-            title="Construction Management Dashboard"
+            src="/construction-ppt/index.html" 
+            title="Construction Management Presentation"
             className="construction-iframe"
             frameBorder="0"
             allowFullScreen
